0ffe8f8360bab7741e8b499eb2b89023b8e72346
Fausto Núñez Alberro <fausto.nunez@outlook.com>
2017-07-04 16:55:53 +0200

    ✅ Recover tests for updateEntryWithId

--- src/utils.test.js
+++ src/utils.test.js
@@ -89,3 +89,48 @@ describe('refresh', () => {
     expect(utils.refresh(utils.actionCreator('TY'))().meta.refresh).toBe(true)
   })
 })
+
+describe('updateEntryWithId', () => {
+  const oldEntry = { id: 'ab12449', name: 'troufignole' }
+  const newEntry = { id: 'ab12449', name: 'tartempion' }
+  const unrelatedEntry = { id: '989e22-21', name: 'something else' }
+
+  test('returns an empty array when given an empty array', () => {
+    expect(utils.updateEntryWithId([], newEntry).length).toEqual(0)
+  })
+
+  test('does not change entries with different ids', () => {
+    expect(
+      utils.updateEntryWithId([oldEntry, unrelatedEntry],
+        newEntry)[1].id
+    ).toEqual(unrelatedEntry.id)
+  })
+
+  test('does not change the order of entries', () => {
+    expect(utils.updateEntryWithId(
+      [unrelatedEntry, oldEntry, unrelatedEntry],
+      newEntry)[2].id
+    ).toEqual(unrelatedEntry.id)
+  })
+
+  test('does not change the array length', () => {
+    expect(utils.updateEntryWithId(
+      [unrelatedEntry, oldEntry, unrelatedEntry],
+      newEntry
+    ).length).toEqual(3)
+  })
+
+  test('replaces the old entry with the new one', () => {
+    expect(utils.updateEntryWithId(
+      [unrelatedEntry, oldEntry, unrelatedEntry],
+      newEntry
+    )[1].id).toEqual(newEntry.id)
+  })
+
+  test('does not mutate the old array', () => {
+    const oldArray = [unrelatedEntry, oldEntry, unrelatedEntry]
+    utils.updateEntryWithId([unrelatedEntry, oldEntry, unrelatedEntry], newEntry)
+    expect(oldArray[1].id).toEqual(oldEntry.id)
+  })
+})
+
